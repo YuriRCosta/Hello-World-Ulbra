fadbbe50d33712e7898f4a66849a5f5170801e57
Yuri Ramos <yuri.ramos@libercapital.com.br>
2023-10-30 17:29:18 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Importe o arquivo CSS
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> {/* Aplicar a classe CSS 'App' */}
+      <h1>Hello, World!</h1>
     </div>
   );
 }
